afa91a96d920d43168e1e9223308021505f0d0d0
Abiddaris <abiddaris1101@gmail.com>
2024-12-16 13:55:59 +0800

    build(common): Update build script for rpy decompiler

--- common/src/main/java/com/abiddarris/common/renpy/rpy/RpyMain.java
+++ common/src/main/java/com/abiddarris/common/renpy/rpy/RpyMain.java
@@ -4,15 +4,9 @@ import static com.abiddarris.common.renpy.internal.Builtins.None;
 
 import java.io.File;
 import java.io.IOException;
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
 
 public class RpyMain {
     public static void main(String[] args) throws IOException {
-//        System.err.print("Press any key to continue");
-//        System.in.read();
-
         long startTime = System.currentTimeMillis();
         try {
             Rpy.initLoader();
@@ -23,6 +17,5 @@ public class RpyMain {
             System.err.println(System.currentTimeMillis() - startTime);
         }
     }
-
 
 }
