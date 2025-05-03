e5fb74477c961c04eb96c7ac93f568a72311e0ba
Abiddaris <abiddaris1101@gmail.com>
2025-05-03 19:52:11 +0800

    chore(preferences) : Remove unused method in `PreferenceAdapter`

--- preferences/src/main/java/com/abiddarris/common/android/preferences/PreferenceAdapter.java
+++ preferences/src/main/java/com/abiddarris/common/android/preferences/PreferenceAdapter.java
@@ -20,42 +20,39 @@ import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView.Adapter;
 import androidx.recyclerview.widget.RecyclerView.ViewHolder;
+
 import com.abiddarris.common.android.preferences.databinding.LayoutPreferenceAdapterBinding;
+
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class PreferenceAdapter extends Adapter<PreferenceAdapter.PreferenceHolder> {
 
-    private Context context;
-    private LayoutInflater inflater;
-    private Preference[] preferences;
+    private final LayoutInflater inflater;
+    private final Preference[] preferences;
 
     PreferenceAdapter(Context context, Preference[] preferences) {
-        this.context = context;
-        
         List<Preference> preferencesList = new ArrayList<>();
         for(Preference preference : preferences) {
             preferencesList.add(preference);
-            
-            if(preference instanceof PreferenceCategory) {
+            if (preference instanceof PreferenceCategory) {
                 PreferenceCategory category = (PreferenceCategory)preference;
-                for(Preference preference0 : category.getPreferences()) {
-                    preferencesList.add(preference0);
-                }
+                Collections.addAll(preferencesList, category.getPreferences());
             }
         }
         
         this.preferences = preferencesList.toArray(new Preference[0]);
-        
-        inflater = LayoutInflater.from(context);
+        this.inflater = LayoutInflater.from(context);
     }
 
+    @NonNull
     @Override
-    public PreferenceHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
-        LayoutPreferenceAdapterBinding binding = LayoutPreferenceAdapterBinding.inflate(inflater);
-        return new PreferenceHolder(binding.layout);
+    public PreferenceHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        return new PreferenceHolder(LayoutPreferenceAdapterBinding.inflate(inflater).layout);
     }
 
     @Override
@@ -71,19 +68,10 @@ public class PreferenceAdapter extends Adapter<PreferenceAdapter.PreferenceHolde
     public int getItemCount() {
         return preferences.length;
     }
-    
-    void refresh(Preference preference) {
-        for(int i = 0; i < preferences.length; i++) {
-            if(preferences[i] == preference) {
-                notifyItemChanged(i);
-                return;
-            }
-        }
-    }
-    
+
     public static class PreferenceHolder extends ViewHolder {
         
-        private LinearLayout layout;
+        private final LinearLayout layout;
         
         public PreferenceHolder(LinearLayout layout) {
             super(layout);
